28907e69600a87fd9079b76f83ac5ccd60885576
itachi_hitachi <yusenng88@gmail.com>
2023-05-07 21:34:46 +0800

    Feat: clickevent with js that link to the next page

---
+++ signup.js
@@ -0,0 +1,4 @@
+document.querySelector("#submit1")
+submit1.addEventListener('click', function (e) {
+    location.href = "file:///C:/Users/aLASTAIR/Documents/signupformproject/signupform/login.html";
+})
