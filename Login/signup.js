700811995fecac3ed5755e4dc14c6c9ac6953ca7
itachi_hitachi <yusenng88@gmail.com>
2023-05-08 19:39:06 +0800

    feat: Register button go to login page when click

---
+++ Login/signup.js
@@ -0,0 +1,21 @@
+const submitButton1 = document.querySelector("#submit1");
+if (submitButton1) {
+    submitButton1.addEventListener('click', function (e) {
+        location.href = "file:///C:/Users/aLASTAIR/Documents/signupformproject/signupform/Login/login.html";
+    });
+}
+
+const submitButton2 = document.querySelector("#submit2");
+if (submitButton2) {
+    submitButton2.addEventListener('click', function (e) {
+        location.href = "file:///C:/Users/aLASTAIR/Documents/signupformproject/signupform/Login/signup.html";
+    });
+}
+
+
+const submitButton3 = document.querySelector("#back");
+if (submitButton3) {
+    submitButton3.addEventListener('click', function (e) {
+        location.href = "file:///C:/Users/aLASTAIR/Documents/signupformproject/signupform/Login/Registration.html";
+    });
+}
