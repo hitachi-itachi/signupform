f40482d4bd4cf5dea4ac6f3d33ff5b5eab22754c
itachi_hitachi <yusenng88@gmail.com>
2023-05-08 19:39:06 +0800

    feat: Created a registration form

--- signup.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("#submit1")
-submit1.addEventListener('click', function (e) {
-    location.href = "file:///C:/Users/aLASTAIR/Documents/signupformproject/signupform/login.html";
-})
